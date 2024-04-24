ead3c1daf8af0734702a11c456b9c042674c63b0
Hatice Dilmac <hdilmac@tederga.com>
2024-04-24 12:15:57 +0300

    [+] Takvimde yer alan meetinge tıklama özelliği getirilerek boş dolu durumu eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,33 @@ class _CalenderWidgetState extends State<CalenderWidget> {
     final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
     final DateTime endTime = startTime.add(const Duration(hours: 2));
     meetings.add(Meeting(
-      eventName: 'Hatice Dilmac Fitness',
+      //if
+      eventName: 'Hatice Fitness',
       from: startTime,
       to: endTime,
-      background: Colors.pink,
+      background: const Color.fromARGB(255, 5, 45, 138),
       isAllDay: false,
     ));
     meetings.add(Meeting(
       eventName: 'Ayşe  Workout B',
       from: DateTime(today.year, today.month, today.day, 10),
       to: startTime.add(const Duration(hours: 2)),
-      background: Colors.blue,
+      background: const Color.fromARGB(255, 5, 45, 138),
       isAllDay: false,
     ));
     meetings.add(Meeting(
       eventName: 'Ahmet Swimming',
       from: startTime,
       to: endTime,
-      background: Colors.green,
+      background: const Color.fromARGB(255, 5, 45, 138),
+      isAllDay: false,
+    ));
+    meetings.add(Meeting(
+      //if event null ,event color yellow
+      eventName: '',
+      from: startTime,
+      to: endTime,
+      background: const Color.fromARGB(255, 196, 189, 86),
       isAllDay: false,
     ));
     return meetings;
@@ -83,18 +92,59 @@ class _CalenderWidgetState extends State<CalenderWidget> {
                   child: const Text('Day View')),
             ],
           ),
+          // Expanded(
+          //   child: SfCalendar(
+          //     initialSelectedDate: DateTime.now(),
+          //     controller: calendarController,
+          //     view: CalendarView.month,
+          //     cellBorderColor: Colors.green,
+          //     dataSource: MeetingDataSource(_getDataSource()),
+          //     // selectionDecoration: BoxDecoration(color: Colors.amber),
+          //     monthViewSettings: const MonthViewSettings(
+          //         appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
+          //         showAgenda:
+          //             true), //haber açılım kısmını gösterir - tıklanan kısmın açıklaması
+          //     blackoutDates: [
+          //       DateTime.now().subtract(const Duration(hours: 48)),
+          //       DateTime.now().subtract(const Duration(hours: 24)),
+          //     ],
+          //   ),
+          // ),
           Expanded(
             child: SfCalendar(
               initialSelectedDate: DateTime.now(),
               controller: calendarController,
-              view: CalendarView.month,
+              view: calendarView,
               cellBorderColor: Colors.green,
               dataSource: MeetingDataSource(_getDataSource()),
-              // selectionDecoration: BoxDecoration(color: Colors.amber),
               monthViewSettings: const MonthViewSettings(
-                  appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
-                  showAgenda:
-                      true), //haber açılım kısmını gösterir - tıklanan kısmın açıklaması
+                appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
+                showAgenda: true,
+              ),
+              onTap: (CalendarTapDetails details) {
+                if (details.targetElement == CalendarElement.appointment) {
+                  final Meeting meeting = details.appointments![0];
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Meeting Details'),
+                        content: Text('Event Name: ${meeting.eventName}\n'
+                            'Start Time: ${meeting.from.toString()}\n'
+                            'End Time: ${meeting.to.toString()}'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
               blackoutDates: [
                 DateTime.now().subtract(const Duration(hours: 48)),
                 DateTime.now().subtract(const Duration(hours: 24)),
